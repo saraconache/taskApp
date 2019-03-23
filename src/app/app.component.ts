f2c30e0ce7794e19fe0d163ebbc3aa9420443e03
Sarah Gomez <sarahvgomez@gmail.com>
2019-03-23 15:52:07 -0600

    Listing objects
    List different objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taskApp';
+  
+
   newTaskName = '';
-  tasks = [];
+ 
+  taskOne = {
+    title : 'clean bathroom',
+    description: 'clean up day',
+    status: 'in-progress',
+    time: 0
+  };
+  
+  taskTwo= {
+    title : 'finish essay',
+    description: 'production company',
+    status: 'completed',
+    time: 0
+  };
+  taskThree = {
+    title : 'do yoga',
+    description: 'go tot he par',
+    status: 'planning',
+    time: 0
+  };
+
+  taskList = [ this.taskOne, this.taskTwo, this.taskThree];
 
   addTask(item) {
-    this.tasks.push(item);
+    this.taskList.push(item);
     this.newTaskName = '';
   }
 }
-
